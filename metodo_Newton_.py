21da328ca36e2f6e8f4656494b27ae1bbd12ada0
OmeGa-2B <bjsanab6@gmail.com>
2024-03-18 19:22:54 -0600

    Actualizacion: metodo newton

--- metodo_Newton_.py
+++ metodo_Newton_.py
@@ -1,15 +1,9 @@
-# Punto fijo
-# OmeGa2B
-
 from sympy import *
 import os
 
 def verificacion_funcion_syntaxis():
     while True:
         try:
-            # sen = sin
-            # e = exp()
-            # pi = pi
             ecuacion = input("\nIngrese la ecuacion con la cual se va a trabajar: ")
             #parse_expr
             return sympify(ecuacion)
@@ -20,49 +14,50 @@ def verificacion_funcion_syntaxis():
             print("\nPor favor intentelo de nuevo")
             print("\n-------------------------------")
 
-def iteracion_punto_fijo(funcion,aproximacion_inicial, Iteraciones_maximas,tol,count):
+def valor_aproximacion():
+    va = float(input("Ingrese la aproximacion inicial: "))
+    return va
 
+def metodo_newton(funcion, aproximacion, iteraciones_maximas,count,tol):
     x = Symbol('x')
     # = funcion.subs(x,a).evalf
     while True:
         try:
-            p = N(funcion.subs(x,aproximacion_inicial))
+            #p = p0 -f(p0)/f'(p0)
+            f_p = N(funcion.subs(x,aproximacion))
+            df = diff(funcion,x)
+            df_p = N(df.subs(x,aproximacion))
+            p = aproximacion-f_p/df_p
             break
         except Exception as e:
             print("\n-------------------------------")
             print("\nSe ha producido un error: ",e)
             print("\nIntente con otro valor de aproximacion")
-            aproximacion_inicial = valor_aproximacion()
+            aproximacion = valor_aproximacion()
             print("\n-------------------------------")
 
     archivo.write('\n\n-----------------------------------------------------------------------------------------')
-    archivo.write("\np0: {}".format(aproximacion_inicial))
+    archivo.write("\np0: {}".format(aproximacion))
     archivo.write("\np: {}".format(p))
-    if count < Iteraciones_maximas:
-        if abs(p-aproximacion_inicial) < tol:
+
+    if count < iteraciones_maximas:
+        if abs(p-aproximacion) < tol:
             return p
         else:
-            return iteracion_punto_fijo(funcion,p,Iteraciones_maximas,1e-6,count+1)
+            return metodo_newton(funcion,p,iteraciones_maximas,count+1,1e-6)
 
     print("\nEl metodo fallo despues de {} iteraciones".format(count))
-    return
-
-
-def valor_aproximacion():
-    va = float(input("Ingrese la aproximacion inicial: "))
-    return va
-
+    return 0
 
 if __name__ == '__main__':
-    #creacion de la ruta donde se guardara el txt con los datos de las iteraciones y de los errores relativos
-    name = 'Punto_fijo.txt'
-    dirc = ''#direccion donde se guardara el archivo
+    name = 'Metodo_Newton.txt'
+    dirc = 'direccion del archivo'
     dirc_completa = os.path.join(dirc, name)
     archivo = open(dirc_completa, 'w')
 
     funcion = verificacion_funcion_syntaxis()
     aproximacion_inicial = valor_aproximacion()
-    Iteraciones_maximas = int(input('Ingrese el numero maximo de interacions: '))
-    p = iteracion_punto_fijo(funcion,aproximacion_inicial,Iteraciones_maximas,1e-6,0)
+    Iteraciones_maximas = int(input('Ingrese el umero maximo de iteraciones: '))
+    p = metodo_newton(funcion,aproximacion_inicial,Iteraciones_maximas,0,tol = 1e-6)
     archivo.close()
     print("\nprocedimiento realizado con exito \nsolucion: {}".format(p))
